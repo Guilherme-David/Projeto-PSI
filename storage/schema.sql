2d68f7260296a68a6053133b9f9443a11efd4e3e
Daviddutra07 <daviddutrags@gmail.com>
2025-07-26 17:49:06 -0300

    Incremento da tabela de produtos

--- storage/schema.sql
+++ storage/schema.sql
@@ -1,9 +1,16 @@
 create table if not exists tb_usuarios (
-    id INTEGER PRIMARY KEY AUTOINCREMENT,
-    email TEXT NOT NULL UNIQUE,
-    senha TEXT NOT NULL,
-    is_admin BOOLEAN NOT NULL DEFAULT 0
+    usr_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usr_email TEXT NOT NULL UNIQUE,
+    usr_senha TEXT NOT NULL,
+    usr_is_admin BOOLEAN NOT NULL DEFAULT 0
 );
 
-ALTER TABLE tb_usuarios ADD COLUMN nome TEXT;
-ALTER TABLE tb_usuarios ADD COLUMN cpf TEXT;
+ALTER TABLE tb_usuarios ADD COLUMN usr_nome TEXT;
+ALTER TABLE tb_usuarios ADD COLUMN usr_cpf TEXT;
+
+CREATE TABLE if not exists tb_produtos (
+    pro_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    pro_nome TEXT NOT NULL,
+    pro_preco TEXT NOT NULL,
+    pro_url_imagem TEXT NOT NULL 
+);
