0fa4a54c874c4a145891f8fbfa2a6c586649963c
Daviddutra07 <daviddutrags@gmail.com>
2025-07-26 17:48:33 -0300

    Incremento do cadastro de administrador

--- storage/iniciar.py
+++ storage/iniciar.py
@@ -1,14 +1,21 @@
 import sqlite3
+from werkzeug.security import generate_password_hash
 
 BANCO='storage/schema.sql'
 
 # conectar com o banco
 conexao = sqlite3.connect('storage/banco.db')
 
+senha_admin =  generate_password_hash(input("Digite a senha do admin: "))
+
 # executar a criação tabela
 with open(BANCO) as f:
     conexao.executescript(f.read())
 
+sql = "INSERT INTO tb_usuarios (usr_email, usr_senha, usr_is_admin) VALUES (?, ?, ?)"
+
+conexao.execute(sql, ("admin@admin.com", senha_admin, True))
+conexao.commit()
 conexao.close()
 # fechar conexao
 
